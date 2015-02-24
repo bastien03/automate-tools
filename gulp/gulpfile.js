d1b61682b6d44209659b36f2d6fcf8572c36bef7
bastien03 <bastienmary@gmail.com>
2015-02-24 23:44:10 +0100

    :construction:

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
 	console.log('1. `gulp.task()` -> create a task');
 	console.log('2. `gulp.run()` -> run a task');
+	console.log('3. `gulp.watch()` -> watch files');
+	console.log('4. `gulp.src()` -> matchs files');
+	console.log('5. `gulp.dest()` -> write files');
 });
 
 gulp.task('run', function() {
 	run('echo Hello World').exec();
+})
+
+gulp.task('watch', function() {
+	watch('gulpfile.js')
+		.pipe(run('echo gulpfile.js has been changed')).exec()
+})
+
+gulp.task('src-dest', function() {
+	gulp.src('app/index.html')
+		.pipe(gulp.dest('build/'));
 })
